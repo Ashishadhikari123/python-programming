4830b43f0ff94a93c63acbb57da0ea4985631032
ASHISH ADHIKARI <56301864+Ashishadhikari123@users.noreply.github.com>
2022-01-06 11:24:55 +0530

    program to swap two numbers

---
+++ swap.py
@@ -0,0 +1,5 @@
+a=int(input("Enter first number "))
+b=(input("Enter second number "))
+print(f"Before swapping value of a is {a} and value of b is {b}")
+a,b=b,a
+print(f"After swapping value of a is {a} and value of b is {b}")
