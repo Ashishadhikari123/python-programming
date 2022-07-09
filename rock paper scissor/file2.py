df80a0abd12d4c1eba2bb1e121ccde8cab8865c9
ASHISH ADHIKARI <56301864+Ashishadhikari123@users.noreply.github.com>
2022-07-09 22:30:25 +0530

    rock paper scissor game

---
+++ rock paper scissor/file2.py
@@ -0,0 +1,61 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+user = int(input("what do u choose? type 0 for ROCK type 1 for PAPER type 2 for SCISSOR\n"))
+print("you choose")
+print(game_images[user])
+x = random.randint(0,2)
+systemChoose = game_images[x]
+print("computer choose")
+print(game_images[x])
+
+# if user choose rock
+if user == 0 and systemChoose == 0:
+    print("tie")
+elif user == 0 and systemChoose == 1:
+    print("you lose")
+elif user == 0 and systemChoose == 2:
+    print("you win")
+
+# if user choose paper
+elif user == 1 and systemChoose == 0:
+    print("you lose")
+elif user == 1 and systemChoose == 1:
+    print("tie")
+elif user == 1 and systemChoose == 3:
+    print("you lose")
+
+# if user choose scissor
+elif user == 2 and systemChoose == 0:
+    print("you lose")
+elif user == 2 and systemChoose == 1:
+    print("you win")
+else :
+    print("tie")
