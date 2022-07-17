43d174b4b9e425a044e4b31d8237a40a304fa2c0
ASHISH ADHIKARI <56301864+Ashishadhikari123@users.noreply.github.com>
2022-07-17 20:35:14 +0530

    loop through a dictionary

---
+++ dict.py
@@ -0,0 +1,8 @@
+friends = {}
+friends["keshav"] = "good friend"
+friends["gaurav"] = "room mate"
+friends["ekansh"] = "kcm school friend"
+friends["ritik"] = "kcm school friend"
+for keys in friends:
+    print(keys,end=": ")
+    print(friends[keys])
